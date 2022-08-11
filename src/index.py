540ce7364e13ec395f9e22fb292f72c27e3ca9f4
Eero Jantunen <jantunen.eero@gmail.com>
2022-08-11 22:39:30 +0300

    tiedostojen koko vertailu lisätty

--- src/index.py
+++ src/index.py
@@ -1,10 +1,11 @@
 import sys
+import os
 from huffman_algo import HuffmanAlgo
 from lz_seventyeight import LzSeventyeight
 
 def main():
     """Käynnistää sovelluksen ja vastaa nyt sovelluslogiikasta.
-    Kovin raakile vielä tässä vaiheessa.
+    Kovin raakile vielä tässä vaiheessa, ei esim. virheen tarkistusta.
     """
 
     selection = str(input("Haluatko pakata vai purkaa tiedoston?" +
@@ -20,28 +21,39 @@ def main():
         with open(file_huffman, "wb") as binary_file:
             binary_file.write(tree)
             binary_file.write(encoding)
-        print("Huffman algoritmilla pakattu tiedosto löytyy polusta:\n" + file_huffman)
+        print("\nHuffman algoritmilla pakattu tiedosto löytyy polusta:\n" + file_huffman)
         #lz78 pakkaus
         file_lz = file_path[:-4] + "_lz" + ".bin"
         lz_seventyeight = LzSeventyeight(data)
         compressed = lz_seventyeight.compress()
         with open(file_lz, "wb") as binary_file:
             binary_file.write(compressed)
-        print("Lempel Ziv 78 algoritmilla pakattu tiedosto löytyy polusta:\n" + file_lz)
+        print("\nLempelZiv78 algoritmilla pakattu tiedosto löytyy polusta:\n" + file_lz)
+        #vertailu
+        org_size = os.path.getsize(file_path)
+        huff_size = os.path.getsize(file_huffman)
+        lz_size = os.path.getsize(file_lz)
+        print("\nAlkuperäisen tiedoston koko on:", org_size, "tavua")
+        print("Huffman algoritmilla pakatun tiedoston koko on:", huff_size, "tavua")
+        print("Huffman -tiedosto on kooltaan", round((huff_size/org_size)*100, 2),
+              "prosenttia alkuperäisestä tiedostosta")
+        print("LempelZiv78 algoritmilla pakatun tiedoston koko on:", lz_size, "tavua")
+        print("LempelZiv78 -tiedosto on kooltaan", round((lz_size/org_size)*100, 2),
+              "prosenttia alkuperäisestä tiedostosta")
 
     elif selection == "2":
-        decompress_algo = str(input("Puretaanko Huffman vai LZ78 algoritmilla pakattu tiedosto" +
+        selection = str(input("Puretaanko Huffman vai LZ78 algoritmilla pakattu tiedosto" +
                           "\n1 = Huffman\n2 = LZ78\n"))
         file_path = str(input("Syötä purettavan tiedoston polku: "))
         with open(file_path, "rb") as file:
             data = file.read()
-        #Huffman purku
-        if decompress_algo == "1":
+        #huffman purku
+        if selection == "1":
             huffman_algo = HuffmanAlgo(data)
             print("Huffman pakkauksesta purettu teksti:")
             print(huffman_algo.huffman_decoding())
         #lz78 purku
-        elif decompress_algo == "2":
+        elif selection == "2":
             lz_seventyeight = LzSeventyeight(data)
             print("LZ78 pakkauksesta purettu teksti:")
             print(lz_seventyeight.decompress())
